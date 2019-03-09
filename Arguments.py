2bfcab3efdc98e8e25b50f8edf7c740c92486c5e
xtx <xtx17@mails.tsinghua.edu.cn>
2019-03-09 16:27:23 +0800

    Mar9 16:27

--- Arguments.py
+++ Arguments.py
@@ -1,3 +1,8 @@
+import os
+import random
+import json
+
+# some constants about pygame
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
@@ -5,42 +10,50 @@ green = (0, 255, 0)
 blue = (0, 0, 255)
 yellow = (255, 255, 0)
 
-density_of_wall = 0.45
-max_num_of_wall = 15
-room_size = 20
+# load the map
+map_dir = "." + os.sep + "Maps" + os.sep
+map_names = os.listdir(map_dir)
+map_names.sort()
+map_id = random.randint(0, len(map_names) - 1)
+map_name = map_names[map_id]
+with open(map_dir + map_name)as file:
+    JSON = file.read()
+    JSON = json.loads(JSON)
+width = JSON["width"]
+height = JSON["height"]
+faction_number = JSON["faction_number"]
+birth_places = JSON["birth_places"]
+ball_places = JSON["ball_places"]
+target_places = JSON["target_places"]
+walls = JSON["walls"]
 
-
-height_of_screen = 600
-width_of_screen = 800
-
-map_lbx = 0
-map_ubx = width_of_screen
-map_lby = 0
-map_uby = height_of_screen
-
-
+# some constants about time
 frames_per_second = 20
+frames_of_game = JSON["time_of_game"] * frames_per_second
+time_of_round = 1.0 / frames_per_second
+frames_of_death = 5 * frames_per_second
+
+
+# some constants about human
 human_hp = 100
 human_meteor_number = 3
+human_flash_number = 3
 human_fireball_interval = frames_per_second * 1
 human_meteor_interval = frames_per_second * 3
-human_radius = 10
-human_speed_max = 5
-human_rotate_max = 10
+human_flash_interval = frames_per_second * 3
+human_velocity = 5
 
-velocity_of_fireball = 4
+# some constants about skills
+fireball_velocity = 4
 fireball_radius = 5
-splash_radius = fireball_radius + 2 * human_speed_max
-fireball_hurt = 10
+splash_radius = fireball_radius + 2 * human_velocity
+splash_hurt = 10
 
-cast_distance = 300
+meteor_distance = 300
 meteor_delay = frames_per_second * 2
-
 explode_radius = 50
 explode_hurt = 50
 
-ball_radius = 5
+flash_distance = 200
 
-time_of_game = 15 * frames_per_second
-time_of_round = 1.0 / frames_per_second
-time_of_death = 5 * frames_per_second
+ball_radius = 5
